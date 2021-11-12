3b1bd37c300b9d99675e3e97245fef27249f99e9
Matheus Aguiar <first_mf@hotmail.com>
2021-11-12 17:28:30 -0300

    first comit

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from "express";
+
+import { CategoryRepository } from "../repositories/CategoriesRepository";
+
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoryRepository();
+
+categoriesRoutes.post("/", (request, response) => {
+    const { name, description } = request.body;
+
+    const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+        return response
+            .status(400)
+            .json({ message: "Category Already exists!" });
+    }
+
+    categoriesRepository.create({ name, description });
+
+    return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+    const all = categoriesRepository.list();
+
+    return response.json(all);
+});
+export { categoriesRoutes };
