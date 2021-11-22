43f9c1ad0a26a8ed6de45b7667f50de985e0b092
Matheus Aguiar <first_mf@hotmail.com>
2021-11-22 15:22:29 -0300

    add Swagger UI

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(express.json());
 
